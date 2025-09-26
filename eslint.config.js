3f07e137abee399bc481ce07942cdd70d0e19881
Takanobu Suzuki <nobublack555@gmail.com>
2025-09-26 12:09:51 -0700

    add eslint and prettier setting

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,61 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js"
+import { defineConfig, globalIgnores } from "eslint/config"
+import prettier from "eslint-config-prettier"
+import importPlugin from "eslint-plugin-import"
+import reactHooks from "eslint-plugin-react-hooks"
+import reactRefresh from "eslint-plugin-react-refresh"
+import unusedImports from "eslint-plugin-unused-imports"
+import globals from "globals"
+import tseslint from "typescript-eslint"
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(["dist"]),
   {
-    files: ['**/*.{ts,tsx}'],
+    files: ["**/*.{js,jsx,ts,tsx}"],
     extends: [
       js.configs.recommended,
+      prettier,
+      importPlugin.configs.typescript,
       tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
+      reactHooks.configs["recommended-latest"],
       reactRefresh.configs.vite,
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parser: tseslint.parser,
+    },
+    plugins: {
+      import: importPlugin,
+      "@typescript-eslint": tseslint.plugin,
+      "unused-imports": unusedImports,
+    },
+    rules: {
+      "import/order": [
+        "error",
+        {
+          groups: ["builtin", "external", "parent", "sibling", "index", "object", "type"],
+          pathGroups: [
+            {
+              pattern: "{react}",
+              group: "builtin",
+              position: "before",
+            },
+            {
+              pattern: "@src/**",
+              group: "parent",
+              position: "before",
+            },
+          ],
+          pathGroupsExcludedImportTypes: ["builtin"],
+          alphabetize: {
+            order: "asc",
+          },
+        },
+      ],
+      "@typescript-eslint/no-unused-vars": "error",
+      "unused-imports/no-unused-imports": "error",
+      "@typescript-eslint/no-explicit-any": "warn",
     },
   },
 ])
